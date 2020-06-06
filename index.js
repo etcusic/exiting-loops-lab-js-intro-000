3482f616b122d9482e917497fcb2564183dc31c4
Eli Cusic <tntitans31@gmail.com>
2020-06-06 17:08:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function breakOut(array, changeValue, stopValue){
 function keepGoing(array, changeValue, skipValue){
   for (var i = 0; i < array.length; i++) {
     if(array[i] !== skipValue){
-      array[i] === changeValue
+      array[i] = changeValue
     }
   }
   return array
